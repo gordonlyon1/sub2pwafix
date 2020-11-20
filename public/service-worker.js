a5f0a31d976b59ca0c814783b6de3cae1e2bc46a
Tri Kusumawardana <trikusumawardana6@gmail.com>
2020-11-20 10:18:08 +0700

    PWA3SUB

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,32 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+if (workbox)
+    console.log(`Workbox berhasil dimuat`);
+else
+    console.log(`Workbox gagal dimuat`);
+
+workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: '1'},
+    { url: '/nav.html', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/push.js', revision: '1'},
+    { url: '/manifest.json', revision: '1'},
+    { url: '/detail.html', revision: '1'},
+    { url: '/js/api.js', revision: '1'},
+    { url: '/js/db.js', revision: '1'},
+    { url: '/js/idb.js', revision: '1'},
+    { url: '/js/index.js', revision: '1'},
+    { url: '/js/nav.js', revision: '1'},
+]);
+
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
+
 const CACHE_NAME = "infobola";
 const urlsToCache = [
   "/",
