b9bf25ad83e37794a07f04410447f52507aa024b
Tri Kusumawardana <trikusumawardana6@gmail.com>
2020-11-20 12:49:54 +0700

    sub3pwafix

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,94 +18,38 @@ workbox.precaching.precacheAndRoute([
     { url: '/js/idb.js', revision: '1'},
     { url: '/js/index.js', revision: '1'},
     { url: '/js/nav.js', revision: '1'},
-]);
+    { url: '/pages/contact.html', revision: '1'},
+    { url: '/pages/klasmen.html', revision: '1'},
+    { url: '/pages/teams.html', revision: '1'},
+    { url: '/pages/saved.html', revision: '1'},
+    { url: '/icon.png', revision: '1'},
+    { url: '/icons/icon-72x72.png', revision: '1'},
+    { url: '/icons/icon-96x96.png', revision: '1'},
+    { url: '/icons/icon-128x128.png', revision: '1'},
+    { url: '/icons/icon-144x144.png', revision: '1'},
+    { url: '/icons/icon-152x152.png', revision: '1'},
+    { url: '/icons/icon-192x192.png', revision: '1'},
+    { url: '/icons/icon-384x384.png', revision: '1'},
+    { url: '/icons/icon-512x512.png', revision: '1'},
+    { url: 'https://fonts.googleapis.com/icon?family=Material+Icons', revision: '1'},
+    { url: 'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2', revision: '1'},
+], {
+ignoreUrlParametersMatching: [/.*/]
+});
 
 workbox.routing.registerRoute(
-    new RegExp('/pages/'),
-    workbox.strategies.staleWhileRevalidate({
-        cacheName: 'pages'
-    })
+  new RegExp('/pages/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'pages'
+  })
 );
 
-const CACHE_NAME = "infobola";
-const urlsToCache = [
-  "/",
-  "/manifest.json",
-  "/nav.html",
-  "/index.html",
-  "/detail.html",
-  "/pages/contact.html",
-  "/pages/klasemen.html",
-  "/pages/teams.html",
-  "/pages/saved.html",
-  "/css/materialize.min.css",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/api.js",
-  "/js/index.js",
-  "/icon.png",
-  "/js/db.js",
-  "/js/idb.js",
-  "/icons/icon-72x72.png",
-  "/icons/icon-96x96.png",
-  "/icons/icon-128x128.png",
-  "/icons/icon-144x144.png",
-  "/icons/icon-152x152.png",
-  "/icons/icon-192x192.png",
-  "/icons/icon-384x384.png",
-  "/icons/icon-512x512.png",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
-];
-
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
+workbox.routing.registerRoute(
+    new RegExp('(https://|http://)'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'data-api'
     })
-  );
-});
-
-
-self.addEventListener("fetch", function (event) {
-  const base_url = "https://api.football-data.org/v2";
-  const online = navigator.onLine;
-  if (event.request.url.includes(base_url) && online){
-    event. respondWith(
-      caches.open(CACHE_NAME).then(function (cache) {
-        return fetch(event.request).then(function(response){
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, {'ignoreSearch': true}).then(function(response){
-        return response || fetch (event.request);
-      })
-    )
-  }
-});
-
-
-self.addEventListener("activate", function (event) {
-  clients.claim();
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
+);
 
 self.addEventListener('push', event => {
   let body;
